9b972ac9ccb150bf29c9becd9d95597c7307d005
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-04-25 13:03:02 -0400

    Initial Commit

---
+++ FinalProjectTests/FinalProjectTests.swift
@@ -0,0 +1,19 @@
+//
+//  FinalProjectTests.swift
+//  FinalProjectTests
+//
+//  Created by Icy on 4/25/26.
+//
+
+import Testing
+@testable import FinalProject
+
+struct FinalProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
